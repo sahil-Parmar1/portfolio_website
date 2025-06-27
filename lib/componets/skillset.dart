20ce4207bcd67c0abeb9cd673b74d7f469ced466
Veloceeo <sahilparmarpalanpur@gmail.com>
2025-06-27 14:12:53 +0530

    skillset was added

--- lib/componets/skillset.dart
+++ lib/componets/skillset.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
-//new github account is added
+
 class Skillset extends StatelessWidget {
   Skillset({super.key});
   final List<Map<String, String>> skills = [
-    {"name": "Flutter", "logo": "assets/logos/flutter.png"},
-    {"name": "Figma", "logo": "assets/logos/figma.png"},
-    {"name": "Firebase", "logo": "assets/logos/firebase.png"},
-    {"name": "Git", "logo": "assets/logos/git.png"},
-    {"name": "Dart", "logo": "assets/logos/dart.png"},
+    {"name": "Flutter", "logo": "assets/logos/diamand.png"},
+    {"name": "Figma", "logo": "assets/logos/flower.png"},
+    {"name": "Firebase", "logo": "assets/logos/halfcircle.png"},
+    {"name": "Git", "logo": "assets/logos/inchworm.png"},
+    {"name": "Dart", "logo": "assets/logos/squish.png"},
   ];
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(8.0),
       child: GridView.builder(
-        shrinkWrap: true,
+       shrinkWrap: true,
           itemCount: 5,
           physics: NeverScrollableScrollPhysics(),
           gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 4,
-               mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-               childAspectRatio: 1
+              maxCrossAxisExtent: 200,
+               mainAxisSpacing: 0,
+              crossAxisSpacing: 0,
+               childAspectRatio: 4,
            ), itemBuilder: (context,index){
           final skill=skills[index];
             return skilltemple(skill["name"] ?? "default", skill["logo"]??"default");
@@ -32,11 +33,14 @@ class Skillset extends StatelessWidget {
 Widget skilltemple(String name,String logo)
 {
   return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.start,
     children: [
-      Text("$name"),
-       Image(image: AssetImage("assets/icons/$logo"),
-       width: 50,
-         height: 50,
+      Text("$name",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 22),),
+       SizedBox(width: 8,),
+       Image(image: AssetImage("$logo"),
+       width: 25,
+         height: 25,
        )
     ],
   );
